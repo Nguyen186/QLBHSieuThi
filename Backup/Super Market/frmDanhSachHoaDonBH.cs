f9596e7a79817c4ee316e4f386b8b80a71e8ee23
Nguyen186 <nguyenbee18062000@gmail.com>
2021-06-20 18:22:19 +0700

    Them database

---
+++ Backup/Super Market/frmDanhSachHoaDonBH.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Super_Market
+{
+    public partial class frmDanhSachHoaDonBH : Form
+    {
+        public frmDanhSachHoaDonBH()
+        {
+            InitializeComponent();
+        }
+
+        private void frmDanhSachHoaDonBH_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
